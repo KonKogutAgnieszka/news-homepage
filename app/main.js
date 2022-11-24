9078bd7e1fdaf1859c466e3691bf211ee713c44c
Agnieszka Koń-Kogut <konagnieszka@gmail.com>
2022-11-24 17:31:00 +0100

    style: menu animation & mixins add

--- app/main.js
+++ app/main.js
@@ -1,8 +1,13 @@
 const hamburger = document.querySelector(".hamburger");
 const overlay = document.querySelector(".overlay");
+const links = document.querySelectorAll(".overlay__links > a");
 
 function toggleMenu() {
+  hamburger.classList.toggle("open");
   overlay.classList.toggle("fade");
 }
 
 hamburger.addEventListener("click", toggleMenu);
+links.forEach((link) => {
+  link.addEventListener("click", toggleMenu);
+});
